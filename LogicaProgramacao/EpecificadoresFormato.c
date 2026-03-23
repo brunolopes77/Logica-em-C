f062081729385c892597f0f49104fc3974b95b61
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-23 09:31:32 -0300

    first commit

---
+++ LogicaProgramacao/EpecificadoresFormato.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    printf("Quanto é o seu salário?\n");
+    
+    printf("Meu salário é: %d.\n", 2300);
+    printf("Meu salário é: %.2f.\n", 2300.64);
+    printf("Meu salário é: %s.\n", "2345");
+    printf("Escreva uma letra: %c.\n", 'A');
+
+    return 0;
+}
--- hello/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main () {
-    printf("Bruno Lopes Borges");
-
-    return 0;
-}
